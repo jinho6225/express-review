95e368af8ce6d09f187cbf3d4bc125cd26cf3226
jinho6225 <jinho6225@hotmail.com>
2020-02-24 12:40:09 -0800

    completed step 2

--- server/controllers.js
+++ server/controllers.js
@@ -1,5 +1,14 @@
-var restaurants = require('../restaurants.js');
+const restaurants = require('../restaurants.json');
 
 const controllers = {
   // TODO
+  getAll: (req, res) => {
+    res.status(200).send(restaurants)
+  },
+  getOne: (req, res) => {
+    let id = req.params.id - 1
+    res.status(200).send(restaurants[id])
+  },
 }
+
+module.exports = controllers
--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
 // TODO
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+const controllers = require('./controllers')
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.use('/', express.static(path.join(__dirname, '../client/dist')))
+
+app.get('/restaurants', controllers.getAll)
+app.get('/restaurants/:id', controllers.getOne)
+
+
+
+app.listen(3003, () => console.log('3003 port is woking'))
